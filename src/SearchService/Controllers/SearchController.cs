fd1a6c282bf250ec248d8739dc0368fc70e18a2e
Julio Hahn <julio.hahn@gmail.com>
2023-11-02 19:59:31 -0400

    AB#1931: Paginating the results

--- src/SearchService/Controllers/SearchController.cs
+++ src/SearchService/Controllers/SearchController.cs
@@ -9,16 +9,25 @@ namespace SearchService.Controllers
     public class SearchController : ControllerBase
     {
         [HttpGet]
-        public async Task<ActionResult<List<Item>>> SearchItems(string searchTerm)
+        public async Task<ActionResult<List<Item>>> SearchItems(string searchTerm, 
+            int pageNumber = 1, int pageSize = 4)
         {
-            var query = DB.Find<Item>();
+            var query = DB.PagedSearch<Item>();
             query.Sort(x => x.Ascending(a => a.Make));
             if (!string.IsNullOrEmpty(searchTerm))
             {
                 query.Match(Search.Full, searchTerm).SortByTextScore();
             }
-            return await query.ExecuteAsync();
-            
+
+            query.PageNumber(pageNumber);
+            query.PageSize(pageSize);
+            var result = await query.ExecuteAsync();
+            return Ok(new
+            {
+                results = result.Results,
+                pageCount = result.PageCount,
+                totalCount = result.TotalCount
+            });
         }
     }
 }
