698dbc98b4b2b2df9950b23ef7c492d389a7ca1f
Julio Hahn <julio.hahn@gmail.com>
2023-11-03 14:57:03 -0400

    AB#1933: Adding Http communication to update the data

--- src/SearchService/Program.cs
+++ src/SearchService/Program.cs
@@ -1,14 +1,12 @@
-using MongoDB.Bson;
-using MongoDB.Driver;
-using MongoDB.Entities;
 using SearchService.Data;
-using SearchService.Models;
+using SearchService.Services;
 
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 
 builder.Services.AddControllers();
+builder.Services.AddHttpClient<AuctionSvcHttpClient>();
 
 var app = builder.Build();
 
