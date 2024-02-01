5008f20fbe4716d9b430fe180f04eb774fc57c02
Julio Hahn <julio.hahn@gmail.com>
2024-01-31 19:35:00 -0500

    AB#2001: Adding remaining contracts and consumers in the auction service

---
+++ src/AuctionService/Consumers/AuctionFinishedConsumer.cs
@@ -0,0 +1,36 @@
+using AuctionService.Data;
+using AuctionService.Entities;
+using Contracts;
+using MassTransit;
+
+namespace AuctionService.Consumers;
+
+public class AuctionFinishedConsumer: IConsumer<AuctionFinished>
+{
+    private readonly AuctionDbContext _dbContext;
+
+    public AuctionFinishedConsumer(AuctionDbContext dbContext)
+    {
+        _dbContext = dbContext;
+    }
+    
+    public async Task Consume(ConsumeContext<AuctionFinished> context)
+    {
+        Console.WriteLine("--> Consuming Auction finished");
+        var auction = await _dbContext.Auctions.FindAsync(context.Message.AuctionId);
+        if (auction != null)
+        {
+            if (context.Message.ItemSold)
+            {
+                auction.Winner = context.Message.Winner;
+                auction.SoldAmount = context.Message.Amount;
+            }
+
+            auction.Status = auction.SoldAmount > auction.ReservePrice
+                ? Status.Finish
+                : Status.ReserveNotMet;
+
+            await _dbContext.SaveChangesAsync();
+        }
+    }
+}
