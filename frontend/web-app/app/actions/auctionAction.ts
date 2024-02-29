f1c2c9161551c02d86c986b71b2d30cad1c65a02
Julio Hahn <julio.hahn@gmail.com>
2024-02-29 18:00:06 -0500

    AB#2042: Protecting routes and test unauthenticated requests

--- frontend/web-app/app/actions/auctionAction.ts
+++ frontend/web-app/app/actions/auctionAction.ts
@@ -7,3 +7,18 @@ export async function getData(query: string): Promise<PagedResult<Auction>> {
     if(!res.ok) throw new Error('Failed to fetch data');
     return res.json();
 }
+
+export async function  UpdateAuctionTest() {
+    const data = {
+        milage: Math.floor(Math.random() * 100000) + 1
+    }
+    const res = await fetch('http://localhost:6001/auctions/afbee524-5972-4075-8800-7d1f9d7b0a0c', {
+        method: 'PUT',
+        headers: {},
+        body: JSON.stringify(data)
+    })
+    
+    if(!res.ok) return {status: res.status, message: res.statusText}
+    
+    return res.statusText;
+}
