c71514db2f9d42bdb01753b3bc24c63e11bdade7
Julio Hahn <julio.hahn@gmail.com>
2024-03-01 07:44:41 -0500

    AB#2045: Filter Auctions by User

--- frontend/web-app/app/nav/UserActions.tsx
+++ frontend/web-app/app/nav/UserActions.tsx
@@ -6,17 +6,32 @@ import {User} from "next-auth";
 import {HiCog, HiUser} from "react-icons/hi2";
 import {AiFillCar, AiFillTrophy, AiOutlineLogout} from "react-icons/ai";
 import {signOut} from "next-auth/react";
+import {usePathname, useRouter} from "next/navigation";
+import {useParamsStore} from "@/hooks/useParamsStore";
 type Props = {
     user: Partial<User>
 }
 export default function UserActions({user}: Props) {
+    const router = useRouter();
+    const pathname = usePathname();
+    const setParams = useParamsStore(state => state.setParams);
+    
+    function setWinner() {
+        setParams({winner: user.username, seller: undefined});
+        if(pathname !== '/') router.push('/');
+    }
+    function setSeller() {
+        setParams({winner: undefined, seller: user.username});
+        if(pathname !== '/') router.push('/');
+    }
+    
     return (
         <Dropdown label={`Welcome ${user.name}`} dismissOnClick={false}>
-            <Dropdown.Item icon={HiUser}>
-                <Link href='/'>My Auctions</Link>
+            <Dropdown.Item icon={HiUser} onClick={setSeller}>
+                My Auctions
             </Dropdown.Item>
-            <Dropdown.Item icon={AiFillTrophy}>
-                <Link href=''>Auctions Won</Link>
+            <Dropdown.Item icon={AiFillTrophy} onClick={setWinner}>
+                Auctions Won
             </Dropdown.Item>
             <Dropdown.Item icon={AiFillCar}>
                 <Link href='/auctions/create'>Sell my car</Link>
--- frontend/web-app/types/next-auth.d.ts
+++ frontend/web-app/types/next-auth.d.ts
@@ -4,11 +4,16 @@ import {Inter} from "next/dist/compiled/@next/font/dist/google";
 declare module 'next-auth' {
     interface Session {
         user: {
+            id: string
             username: string
         } & DefaultSession['user']
     }
     
     interface Profile {
+        username: string
+    }
+    
+    interface User {
         username: string
     }
 }
