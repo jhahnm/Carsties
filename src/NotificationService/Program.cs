d556cf561cefb0acaa96b3ddfb21f0bcb144be33
Julio Hahn <julio.hahn@gmail.com>
2024-03-07 10:39:34 -0500

    AB#2064: Create the notification Service

---
+++ src/NotificationService/Program.cs
@@ -0,0 +1,20 @@
+using MassTransit;
+
+var builder = WebApplication.CreateBuilder(args);
+builder.Services.AddMassTransit(x =>
+{
+    x.SetEndpointNameFormatter(new KebabCaseEndpointNameFormatter("nt", false));
+    x.UsingRabbitMq((context, cfg) =>
+    {
+        cfg.Host(builder.Configuration["RabbitMq:Host"], "/", host =>
+        {
+            host.Username(builder.Configuration.GetValue("RabbitMq:Usename", "guest"));
+            host.Password(builder.Configuration.GetValue("RabbitMq:Password", "guest"));
+        });
+        cfg.ConfigureEndpoints(context);
+    });
+});
+var app = builder.Build();
+
+
+app.Run();
