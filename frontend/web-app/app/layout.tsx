f98d50f17c3a09569b637428afb92dd0ded20a32
Julio Hahn <julio.hahn@gmail.com>
2024-03-08 18:55:55 -0500

    AB#2075: Add signalR to client

--- frontend/web-app/app/layout.tsx
+++ frontend/web-app/app/layout.tsx
@@ -1,7 +1,8 @@
 import type { Metadata } from "next";
 import "./globals.css";
 import Navbar from "./nav/Navbar";
-import ToasterProvider from "@/providers/ToasterProvider";
+import SignalRProvider from "@/app/providers/SignalRProvider";
+import ToasterProvider from "@/app/providers/ToasterProvider";
 
 export const metadata: Metadata = {
   title: "Carsties",
@@ -19,7 +20,9 @@ export default function RootLayout({
           <ToasterProvider />
           <Navbar />
           <main className='container mx-auto px-5 pt-10'>
-              {children}
+              <SignalRProvider>
+                  {children}
+              </SignalRProvider>
           </main>
       </body>
     </html>
